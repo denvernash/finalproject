33699a60e4a78293474674b7d4709b620483f399
denvernash <35231540+denvernash@users.noreply.github.com>
2018-04-14 17:58:36 -0400

    getting wiki data with caching

--- wikidog.py
+++ wikidog.py
@@ -1,5 +1,11 @@
 import requests
 import json
+from bs4 import BeautifulSoup
+
+
+print("***"*20)
+print('\n')
+
 
 CACHE_FNAME = "wiki.json"
 
@@ -13,46 +19,47 @@ except:
     CACHE_DICTION = {}
 
 
+def soup_it(nps):
+    soup = BeautifulSoup(nps, "html.parser")
+    return soup
 
-# caching key to ensure cache doesn't store the same information twice
-def sorted_search_params(baseurl, params, private_keys=["api_key", 'key']):
-    sorted_params = sorted(params.keys())
-    acc = []
-    for item in sorted_params:
-        if item not in private_keys:
-            acc.append("{}-{}".format(item, params[item]))
-    return baseurl + "_".join(acc)
-
-
-# setting up caching
 DUMMY3 = True
 DUMMY4 = True
-
-def get_wiki_data(search):
-    baseurl = 'https://en.wikipedia.org/w/api.php'
-    params = {}
-    params['format'] = 'json'
-    params['action'] = 'query'
-    params['titles'] = search
-    params['prop'] = 'info'
-    # params['rvprop'] = 'content'
-    uniq_id = sorted_search_params(baseurl, params)
-    if uniq_id in CACHE_DICTION:
+def data_cache(search_url):
+    global DUMMY3
+    global DUMMY4
+    if search_url in CACHE_DICTION:
+        data = ((CACHE_DICTION[search_url]))
         if DUMMY3:
             print("Returning data from cache file")
             DUMMY3 = False
-        return CACHE_DICTION[uniq_id]
+        return(soup_it(data))
     else:
-        resp = requests.get(baseurl, params = params, headers= {'User-Agent': "SI507 (https://github.com/denvernash/finalproject; nashd@umich.edu)"}).text
-        data = json.loads(resp)
-        CACHE_DICTION[uniq_id] = data
+        data = requests.get(search_url).text.strip()
+        CACHE_DICTION[search_url] = data
         fname = open(CACHE_FNAME, 'w')
         fname.write(json.dumps((CACHE_DICTION), indent=2))
         fname.close()
         if DUMMY4:
             print("Getting fresh data")
-            DUMMY2 = False
-        return (data)
-    info = json.loads(data)
+            DUMMY4 = False
+        return (soup_it(data))
 
-    print(info.keys())
+def get_wiki_data(search):
+    search_url = 'https://en.wikipedia.org/wiki/{}?action=render'.format(search)
+    # search_url = 'https://en.wikipedia.org/w/index.php?action=raw&title={}'.format(search)
+    wiki_data = data_cache(search_url)
+    return wiki_data
+
+
+
+
+info = get_wiki_data("poodle")
+other = info.find_all('Hypoallergenic qualities')
+
+print(other)
+
+
+
+print('\n')
+print("***"*20)
