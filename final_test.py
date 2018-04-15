894ccf932dd075b27f853ee2a2fe9c16a478ff34
denvernash <35231540+denvernash@users.noreply.github.com>
2018-04-15 14:22:02 -0400

    added test of database

--- final_test.py
+++ final_test.py
@@ -1,4 +1,19 @@
 import unittest
 from final import *
+from nato import *
 
 class TestDatabase(unittest.TestCase):
+
+    def test_dog_table(self):
+        conn = sqlite3.connect(DBNAME)
+        cur = conn.cursor()
+
+        sql = 'SELECT Name FROM Dogs'
+        results = cur.execute(sql)
+        result_list = results.fetchall()
+        self.assertIn(('Wookie!',), result_list)
+        self.assertEqual(len(result_list), 4166)
+
+
+if __name__ == '__main__':
+    unittest.main()
