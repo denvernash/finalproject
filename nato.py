ca712472a79e9a5b16b9bba6783bdf70dcc22f39
denvernash <35231540+denvernash@users.noreply.github.com>
2018-04-15 22:03:41 -0400

    adding geo data for unlisted shelter locations

--- nato.py
+++ nato.py
@@ -129,3 +129,225 @@ LIST_OF_BREEDS = ['Affenpinscher', 'Afghan Hound', 'Airedale Terrier', 'Akbash',
 'Wirehaired Dachshund', 'Wirehaired Pointing Griffon', 'Wirehaired Terrier',
 'Xoloitzcuintle / Mexican Hairless', 'Yellow Labrador Retriever',
 'Yorkshire Terrier Yorkie']
+
+
+
+
+
+
+
+# The geographic information below includes data created by MaxMind, available from
+# http://www.maxmind.com/"
+
+# OPEN DATA LICENSE for MaxMind WorldCities and Postal Code Databases
+#
+# Copyright (c) 2008 MaxMind Inc.  All Rights Reserved.
+#
+# The database uses toponymic information, based on the Geographic Names Data Base, containing official standard names approved by
+# the United States
+# Board on Geographic Names and maintained by the National Geospatial-Intelligence Agency. More information is available at the Maps and
+# Geodata link at www.nga.mil. The National Geospatial-Intelligence Agency name, initials, and seal
+# are protected by 10 United States
+# Code Section 445.
+#
+# It also uses free population data from Stefan Helders www.world-gazetteer.com.
+# Visit his website to download the free population data.  Our database
+# combines Stefan's population data with the list of all cities in the world.
+#
+# All advertising materials and documentation mentioning features or use of
+# this database must display the following acknowledgment:
+# "This product includes data created by MaxMind, available from
+# http://www.maxmind.com/"
+#
+# Redistribution and use with or without modification, are permitted provided
+# that the following conditions are met:
+# 1. Redistributions must retain the above copyright notice, this list of
+# conditions and the following disclaimer in the documentation and/or other
+# materials provided with the distribution.
+# 2. All advertising materials and documentation mentioning features or use of
+# this database must display the following acknowledgement:
+# "This product includes data created by MaxMind, available from
+# http://www.maxmind.com/"
+# 3. "MaxMind" may not be used to endorse or promote products derived from this
+# database without specific prior written permission.
+#
+# THIS DATABASE IS PROVIDED BY MAXMIND.COM ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL MAXMIND.COM BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# DATABASE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+UNLISTED_SHELTER_LOCATIONS = {
+'Decatur, AL': '34.6058333,-86.9833333'
+'Harrison, AR': '36.2297222,-93.1075000'
+'Phoenix, AZ': '33.4483333,-112.0733333'
+'Scottsdale, AZ': '33.5091667,-111.8983333'
+'Inglewood, CA': '33.9616667,-118.3522222'
+'Long Beach, CA': '33.7669444,-118.1883333'
+'Norco, CA': '33.9311111,-117.5477778'
+'Phelan, CA': '34.4261111,-117.5713889'
+'San Diego, CA': '32.7152778,-117.1563889'
+'Aurora, CO': '39.7294444,-104.8313889'
+'Colorado Springs, CO': '38.8338889,-104.8208333'
+'Denver, CO': '39.7391667,-104.9841667'
+'Yoder, CO': '38.8394444,-104.2213889'
+'Wilmington, DE': '39.7291667,-75.5444444'
+'Altamonte Springs, FL': '28.6608333,-81.3658333'
+'Alachua, GA': '29.7513889,-82.4250000'
+'Davenport, IA': '41.5236111,-90.5775000'
+'Ottumwa, IA': '41.0050000,-92.3783333'
+'Champaign, IL': '40.1163889,-88.2433333'
+'Chicago, IL': '41.8500000,-87.6500000'
+'Homer Glen, IL': '41.6000000,-87.9380556'
+'Mount Prospect, IL': '42.0663889,-87.9372222'
+'Oak Park, IL': '41.8850000,-87.7844444'
+'Pontiac, IL': '40.8808333,-88.6297222'
+'Urbana, IL': '40.1105556,-88.2072222'
+'Wheaton, IL': '41.8661111,-88.1069444'
+'Woodridge, IL': '41.7469444,-88.0502778'
+'Cedar Lake, IN': '41.3647222,-87.4411111'
+'Crawfordsville, IN': '40.0411111,-86.8744444'
+'Fillmore, IN': '39.6675000,-86.7533333'
+'Garrett, IN': '41.3494444,-85.1355556'
+'Indianapolis, IN': '39.7683333,-86.1580556'
+'Jeffersonville, IN': '38.2775000,-85.7372222'
+'Metamora, IN': '39.4497222,-85.1394444'
+'Mishawaka, IN': '41.6619444,-86.1586111'
+'Noblesville, IN': '40.0455556,-86.0086111'
+'South Bend, IN': '41.6833333,-86.2500000'
+'Topeka, KS': '39.0483333,-95.6777778'
+'Georgetown, KY': '38.2097222,-84.5588889'
+'Newport, KY': '39.0913889,-84.4958333'
+'Tewksbury, MA': '42.6105556,-71.2347222'
+'Edgewood, MD': '39.4186111,-76.2947222'
+'Elkton, MD': '39.6066667,-75.8336111'
+'Prince Frederick, MD': '38.5402778,-76.5847222'
+'Waldorf, MD': '38.6244444,-76.9394444'
+'Ada, MI': '42.9541667,-85.4888889'
+'Attica, MI': '43.0302778,-83.1661111'
+'Bay City, MI': '43.5944444,-83.8888889'
+'Berkley, MI': '42.5030556,-83.1836111'
+'Canton, MI': '42.3086111,-83.4822222'
+'East Lansing, MI': '42.7369444,-84.4838889'
+'Eaton Rapids, MI': '42.5091667,-84.6558333'
+'Elsie, MI': '43.0886111,-84.3869444'
+'Garden City, MI': '42.3255556,-83.3311111'
+'Grand Rapids, MI': '42.9633333,-85.6680556'
+'Grosse Pointe Park, MI': '42.3758333,-82.9375000'
+'Hastings, MI': '42.6458333,-85.2908333'
+'Holland, MI': '42.7875000,-86.1088889'
+'Howell, MI': '42.6072222,-83.9294444'
+'Kent City, MI': '43.2200000,-85.7511111'
+'Lansing, MI': '42.7325000,-84.5555556'
+'Livonia, MI': '42.3683333,-83.3527778'
+'Muskegon, MI': '43.2341667,-86.2483333'
+'New Boston, MI': '42.1625000,-83.4030556'
+'Newaygo, MI': '43.4197222,-85.8000000'
+'Northville, MI': '42.4311111,-83.4833333'
+'Otsego, MI': '42.4605556,-85.6963889'
+'Oxford, MI': '42.8247222,-83.2647222'
+'St. Clair Shores, MI': '42.4969444,-82.8888889'
+'Ypsilanti, MI': '42.2411111,-83.6130556'
+'Minneapolis, MN': '44.9800000,-93.2636111'
+'Rochester, MN': '44.0216667,-92.4697222'
+'Moscow Mills, MO': '38.9477778,-90.9180556'
+'Raytown, MO': '39.0086111,-94.4633333'
+'Saint Charles, MO': '38.7838889,-90.4811111'
+'Saint Louis, MO': '38.6272222,-90.1977778'
+'Cary, NC': '35.7913889,-78.7813889'
+'Raeford, NC': '34.9808333,-79.2244444'
+'Southern Pines, NC': '35.1738889,-79.3925000'
+'Statesville, NC': '35.7825000,-80.8875000'
+'Boonton, NJ': '40.9025000,-74.4075000'
+'Cape May Court House, NJ': '39.0825000,-74.8241667'
+'Hackettstown, NJ': '40.8538889,-74.8294444'
+'Marlton, NJ': '39.8911111,-74.9222222'
+'Bedford Hills, NY': '41.2366667,-73.6950000'
+'Big Flats, NY': '42.1372222,-76.9372222'
+'Buffalo, NY': '42.8863889,-78.8786111'
+'Cuba, NY': '42.2175000,-78.2755556'
+'Menands, NY': '42.6919444,-73.7250000'
+'North Norwich, NY': '42.6169444,-75.5272222'
+'Spencerport, NY': '43.1863889,-77.8041667'
+'Springville, NY': '42.5083333,-78.6675000'
+'Ashland, OH': '40.8686111,-82.3183333'
+'Avon, OH': '41.4516667,-82.0355556'
+'Bellbrook, OH': '39.6355556,-84.0708333'
+'Cincinnati, OH': '39.1619444,-84.4569444'
+'Cleveland, OH': '41.4994444,-81.6955556'
+'Columbus, OH': '39.9611111,-82.9988889'
+'Cuyahoga Falls, OH': '41.1338889,-81.4847222'
+'Dublin, OH': '40.0991667,-83.1141667'
+'Eastlake, OH': '41.6538889,-81.4505556'
+'Elyria, OH': '41.3683333,-82.1077778'
+'Garfield Heights, OH': '41.4169444,-81.6061111'
+'Hudson, OH': '41.2400000,-81.4408333'
+'Lakewood, OH': '41.4819444,-81.7983333'
+'Marion, OH': '40.5886111,-83.1286111'
+'Marysville, OH': '40.2363889,-83.3672222'
+'New Albany, OH': '40.0811111,-82.8088889'
+'Port Clinton, OH': '41.5119444,-82.9377778'
+'Rootstown, OH': '41.0988889,-81.2427778'
+'Rossford, OH': '41.6097222,-83.5644444'
+'Sharonville, OH': '39.2680556,-84.4133333'
+'Tiffin, OH': '41.1144444,-83.1780556'
+'Toledo, OH': '41.6638889,-83.5552778'
+'Upper Sandusky, OH': '40.8272222,-83.2813889'
+'Wooster, OH': '40.8050000,-81.9352778'
+'Bowmanville, ON': '43.9,-78.683333'
+'Cobourg, ON': '43.966667,-78.166667'
+'Dunnville, ON': '42.9,-79.616667'
+'Guelph, ON': '43.55,-80.25'
+'Hamilton, ON': '43.25,-79.833333'
+'London, ON': '42.983333,-81.25'
+'Mississauga, ON': '43.15,-79.5'
+'North Bay, ON': '46.3,-79.45'
+'Oakville, ON': '43.433333,-79.666667'
+'Oxford Station, ON': '45.016388, -75.645950'
+'Peterborough, ON': '75879,44.3,-78.333333'
+'Toronto, ON': '43.666667,-79.416667'
+'Victoria Harbour, ON': '44.75,-79.766667'
+'Williamsford, ON': '44.3780,-80.8715'
+'Windsor, ON': '48.95,-55.666667'
+'Bend, OR': '44.0583333,-121.3141667'
+'Warrenton, OR': '46.1652778,-123.9225000'
+'Chester Springs, PA': '40.0950000,-75.6172222'
+'Hellertown, PA': '40.5794444,-75.3411111'
+'Irwin, PA': '40.3244444,-79.7013889'
+'Mechanicsburg, PA': '40.2141667,-77.0088889'
+'Mount Joy, PA': '40.1097222,-76.5036111'
+'Northumberland, PA': '40.8916667,-76.7977778'
+'Sharpsville, PA': '41.2591667,-80.4722222'
+'Shippenville, PA': '41.2502778,-79.4597222'
+'State College, PA': '40.7933333,-77.8602778'
+'West Grove, PA': '39.8219444,-75.8277778'
+'Rigaud, QC': '45.466667,-74.3'
+'Saint-Bernard-De-Lacolle, QC': '45.0577,-73.4690'
+'West Columbia, SC': '33.9933333,-81.0741667'
+'Aberdeen, SD': '45.4647222,-98.4861111'
+'Nashville, TN': '36.1725000,-86.7597222'
+'Austin, TX': '30.2669444,-97.7427778'
+'Denton, TX': '33.2147222,-97.1327778'
+'Garland, TX': '32.9125000,-96.6386111'
+'Houston, TX': '29.7630556,-95.3630556'
+'Richardson, TX': '32.9480556,-96.7294444'
+'Van Alstyne, TX': '33.4213889,-96.5769444'
+'Millville, UT': '41.6816667,-111.8222222'
+'Aldie, VA': '38.9755556,-77.6416667'
+'Alexandria, VA': '38.8047222,-77.0472222'
+'Catharpin, VA': '38.8541667,-77.5722222'
+'Huddleston, VA': '37.1566667,-79.4747222'
+'Norfolk, VA': '36.8466667,-76.2855556'
+'Kirkland, WA': '47.6816667,-122.2075000'
+'Appleton, WI': '44.2619444,-88.4152778'
+'Fond Du Lac, WI': '43.7730556,-88.4469444'
+'Kaukauna, WI': '44.2780556,-88.2719444'
+'Kenosha, WI': '42.5847222,-87.8211111'
+'Racine, WI': '42.7261111,-87.7827778'
+}
