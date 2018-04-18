0f1b58daa62ab0616159e1800103d77f6a835f52
denvernash <35231540+denvernash@users.noreply.github.com>
2018-04-17 20:45:45 -0400

    working on getting all dog photos

--- flickrdog.py
+++ flickrdog.py
@@ -17,6 +17,18 @@ except:
 
 
 
+LICENSE_URLS = {
+  "1" : "https://creativecommons.org/licenses/by-nc-sa/2.0/",
+  "2" : "https://creativecommons.org/licenses/by-nc/2.0/",
+  "3" : "https://creativecommons.org/licenses/by-nc-nd/2.0/",
+  "4" : "https://creativecommons.org/licenses/by/2.0/",
+  "5" : "https://creativecommons.org/licenses/by-sa/2.0/",
+  "6" : "https://creativecommons.org/licenses/by-nd/2.0/"
+}
+
+
+
+
 
 
 class Image():
@@ -29,7 +41,6 @@ class Image():
         if len(self.size) > 0:
             self.size = '_' + self.size
         self.image_url = 'https://farm{}.staticflickr.com/{}/{}_{}{}.jpg'.format(self.farm_id, self.server_id, self.img_id, self.secret_id, self.size)
-        self.license = "https://creativecommons.org/licenses/by-nc-sa/2.0/"
         self.license_code = 'https://creativecommons.org/licenses/by-nc-sa/2.0/legalcode'
         self.content_url = "Flickr URL"
         self.username = 'Name'
@@ -40,6 +51,8 @@ class Image():
         self.content_url = attr_data['photo']['urls']['url'][0]['_content']
         self.username = attr_data['photo']['owner']['username']
         self.title = attr_data['photo']['title']['_content']
+        self.license_id = attr_data['photo']['license']
+        self.license_url = LICENSE_URLS[self.license_id]
 
 ############################################################
 #
@@ -47,6 +60,7 @@ class Image():
 #
 #   limit: 30 images per page on application
 #   attribution: "This product uses the Flickr API but is not endorsed or certified by Flickr."
+#   call limit: under 3600 per hour per key
 #
 #
 ############################################################
@@ -64,14 +78,14 @@ def sorted_search_params(baseurl, params, private_keys=["api_key", 'key']):
 def search_photos_params(search, amount = 1):
     params= {}
     params["api_key"] = flickr_key
-    params["tags"] = search
+    params["tags"] = search + ',dog,-finelinerpens,-thehulk,-blancoynegro,-pee,-drawing,-barking,-cat,-postcardsforthelunchbag,-watercolor'
     params["tag_mode"] = "all"
-    params['sort'] = 'relevance'
+    params['sort'] = 'interestingness-desc'
     params['content_type'] = '1'
     params["method"] = "flickr.photos.search"
     params["per_page"] = amount
     params["format"] = 'json'
-    params['license'] = '1'
+    params['license'] = '1,2,3,4,5,6'
     return params
 
 def info_photos_params(photoid, secret):
@@ -84,18 +98,21 @@ def info_photos_params(photoid, secret):
     return params
 
 
-
+CALL_LIMIT = 0
 # get flicker image data from seach parameters for dogs
 def get_flickr_img(params):
     baseurl = "https://api.flickr.com/services/rest/"
-
+    global CALL_LIMIT
     uniq_id = sorted_search_params(baseurl, params)
     if uniq_id in CACHE_FDICTION:
         print("Returning cache data")
         return CACHE_FDICTION[uniq_id]
+    elif CALL_LIMIT >= 3500:
+        sys.exit("You have reached the Flickr call limit per hour")
     else:
         flickr_text = requests.get(baseurl, params = params).text
-        print("Getting fresh data")
+        CALL_LIMIT += 1
+        print(CALL_LIMIT, "Getting fresh data")
         flickr_text_fixed = flickr_text[14:-1]
         flickr_data = json.loads(flickr_text_fixed)
         fname = open(CACHE_FFNAME, 'w')
@@ -128,17 +145,24 @@ def time_delay(number = 15):
 
 def create_dog_images(breed_list, amount = 1, size=''):
     breed_imgs = {}
+    not_found = []
     for breed in breed_list:
-        dog = breed.split("/")[0].split("(")[0].strip()
+        try:
+            dog = breed.split("/")[0].split("(")[0].strip()
+            print(dog)
+            img = create_image(dog, amount, size)[0]
+            print(img.content_url)
+            breed_imgs[breed] = img
+            # time_delay(10)
+        except:
+            not_found.append(breed)
+    print("Not Found \n")
+    for dog in not_found:
         print(dog)
-        img = create_image(dog, amount, size)[0]
-        print(img)
-        breed_imgs[breed] = img
-        # time_delay(10)
     return breed_imgs
 
-BREED_IMGS = create_dog_images(LIST_OF_BREEDS[:1])
-webbrowser.open(BREED_IMGS[LIST_OF_BREEDS[0]].content_url)
+BREED_IMGS = create_dog_images(LIST_OF_BREEDS[:50])
+# webbrowser.open(BREED_IMGS[LIST_OF_BREEDS[0]].content_url)
 
 # image_datum = get_flickr_img(info_photos_params('28426106799', '182b6ee552'))
 # print(image_datum['photo']['license'])
