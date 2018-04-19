4dbd04e6ea3a8ea688a71841fdbc92c2108f8e03
denvernash <35231540+denvernash@users.noreply.github.com>
2018-04-18 20:02:09 -0400

    getting breeds

--- wikidog.py
+++ wikidog.py
@@ -35,16 +35,16 @@ class Breed():
         self.weight = "Unknown"
         self.height = "Unknown"
         if breed_dict != None:
-            self.origin = breed_dict['origin'].title()
-            self.coat = breed_dict['coat'].capitalize()
-            self.color = breed_dict['color'].capitalize()
-            self.life_span = breed_dict['life-span'].capitalize()
-            self.litter_size = breed_dict['litter-size'].capitalize()
-            self.weight = breed_dict['weight'].capitalize()
-            self.height = breed_dict['height'].capitalize()
+            self.origin = breed_dict['origin'].title().strip()
+            self.coat = breed_dict['coat'].capitalize().strip()
+            self.color = breed_dict['color'].capitalize().strip()
+            self.life_span = breed_dict['life-span'].capitalize().strip()
+            self.litter_size = breed_dict['litter-size'].capitalize().strip()
+            self.weight = breed_dict['weight'].capitalize().strip(':').strip("_").strip('~').strip()
+            self.height = breed_dict['height'].capitalize().strip(':').strip("_").strip('~').strip()
 
     def __str__(self):
-        return ('{}, {}, {}, {}, {}, {}, {}'.format(self.kind, self.origin, self.coat, self.color, self.life_span, self.litter_size, self.weight, self.height))
+        return ('{}, {}, {}, {}, {}, {}, {}, {}'.format(self.kind, self.origin, self.coat, self.color, self.life_span, self.litter_size, self.weight, self.height))
 
 
 
@@ -83,63 +83,76 @@ def soup_data_cache(search_url):
         return (soup_it(data))
 
 def get_wiki_data(search):
-    search_url = 'https://en.wikipedia.org/wiki/{}%20dog?action=render'.format(search)
+    search_url = 'https://en.wikipedia.org/wiki/{}?action=render'.format(search)
     wiki_data = soup_data_cache(search_url)
 
     return wiki_data
 
+def create_breed_dictionary(rows):
+    dog_breed = {}
+    dog_breed['origin'] = "Unknown"
+    dog_breed['coat'] = "Unknown"
+    dog_breed['color'] = "Unknown"
+    dog_breed['life-span'] = "Unknown"
+    dog_breed['litter-size'] = "Unknown"
+    dog_breed['weight'] = "Unknown"
+    dog_breed['height'] = "Unknown"
+    for row in rows:
+        if (type(row.th)) == (type(None)):
+            pass
+        elif (type(row.td)) == (type(None)):
+            pass
+        else:
+            heading = str(row.th.text.lower().replace('\n', ' ').replace('\xa0', '-')).split('[')[0]
+            texting = str(row.td.text.lower().replace('\n', ' ').replace('\xa0', '-')).split('[')[0]
+            if heading == "origin":
+                dog_breed[heading] = texting
+            elif heading == 'coat':
+                dog_breed[heading] = texting
+            elif heading == 'color':
+                dog_breed[heading] = texting
+            elif heading == 'colour':
+                dog_breed['color'] = texting
+            elif heading =='life-span':
+                dog_breed[heading] = texting
+            elif heading == 'litter-size':
+                dog_breed[heading] = texting
+            else:
+                pass
+    return dog_breed
 
 
 def create_wiki_dict(breed_list):
     wiki_dict = {}
+    not_found = []
     timer = 0
     for breed in breed_list:
         breed_to_search = breed.split('/')[0].strip()
         info = get_wiki_data(breed_to_search)
-        print(breed_list.index(breed))
-        time_delay(8)
-        timer += 1
-        print("*****"+ str(timer) + "*****")
         try:
-            table = info.find('table', attrs = {'class': 'infobox biota'})
-            rows = table.find_all('tr')
+            try:
+                table = info.find('table', attrs = {'class': 'infobox biota'})
+                rows = table.find_all('tr')
+            except:
+                try:
+                    info = get_wiki_data((breed_to_search + '%20dog'))
+                    table = info.find('table', attrs = {'class': 'infobox biota'})
+                    rows = table.find_all('tr')
+                except:
+                    pass
+            timer = (breed_list.index(breed))
+            timer += 1
+            print("*****"+ str(timer) + "*****")
+            time_delay(8)
+            dog_breed = create_breed_dictionary(rows)
+
             table_text = []
-            dog_breed = {}
-            dog_breed['origin'] = "Unknown"
-            dog_breed['coat'] = "Unknown"
-            dog_breed['color'] = "Unknown"
-            dog_breed['life-span'] = "Unknown"
-            dog_breed['litter-size'] = "Unknown"
-            dog_breed['weight'] = "Unknown"
-            dog_breed['height'] = "Unknown"
             for row in rows:
                 for stuff in row.contents:
                     try:
                         table_text.append((stuff.text).lower())
                     except:
                         pass
-                if (type(row.th)) == (type(None)):
-                    pass
-                elif (type(row.td)) == (type(None)):
-                    pass
-                else:
-                    heading = str(row.th.text.lower().replace('\n', ' ').replace('\xa0', '-')).split('[')[0]
-                    texting = str(row.td.text.lower().replace('\n', ' ').replace('\xa0', '-')).split('[')[0]
-                    if heading == "origin":
-                        dog_breed[heading] = texting
-                    elif heading == 'coat':
-                        dog_breed[heading] = texting
-                    elif heading == 'color':
-                        dog_breed[heading] = texting
-                    elif heading == 'colour':
-                        dog_breed['color'] = texting
-                    elif heading =='life-span':
-                        dog_breed[heading] = texting
-                    elif heading == 'litter-size':
-                        dog_breed[heading] = texting
-                    else:
-                        pass
-
             new_table_text = []
             for item in table_text:
                 more = item.replace(' ', '_').replace('\n', ' ').replace('\xa0', '-').replace('male', '').replace('female',
@@ -152,21 +165,26 @@ def create_wiki_dict(breed_list):
                         cleaner_table_text.append(x)
             if 'weight' in cleaner_table_text:
                 to_index = (cleaner_table_text.index('weight')) + 1
-                dog_breed['weight'] = (cleaner_table_text[to_index])
+                dog_breed['weight'] = (cleaner_table_text[to_index].split('[')[0])
             if 'height' in cleaner_table_text:
                 to_index = (cleaner_table_text.index('height')) + 1
-                dog_breed['height'] = (cleaner_table_text[to_index])
+                dog_breed['height'] = (cleaner_table_text[to_index].split('[')[0])
 
             wiki_dict[breed] = Breed(breed, dog_breed)
         except:
-
+            not_found.append(breed)
+    print(not_found)
     return wiki_dict
 
-DOG_BREED_DICT = create_wiki_dict(LIST_OF_BREEDS[:15])
+x = 85
+y = 100
+DOG_BREED_DICT = create_wiki_dict(LIST_OF_BREEDS[x:y])
 
 for key in DOG_BREED_DICT:
     pass
-
+    # print(DOG_BREED_DICT[key])
+    # print('\n')
+# print(repr(DOG_BREED_DICT['Bullmastiff'].color))
 
 
 
